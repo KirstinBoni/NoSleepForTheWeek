e0fb7d978050e46f51e6a1a38cb7bf0a227b5211
gleske3 <ryangleske@gmail.com>
2018-03-03 21:43:58 -0500

    Initial Structure

---
+++ alarm.py
@@ -0,0 +1,6 @@
+class Alarm:
+    def __init__(self, alarm_enabled = True, alarm_timer = 20, alarm_volume = 4, alarm_sound = ""):
+        self.alarm_enabled = alarm_enabled
+        self.alarm_timer = alarm_timer
+        self.alarm_volume = alarm_volume
+        self.alarm_sound = alarm_sound
