d83e3eafb82288240c35d32d00c1f7ed37cc71f3
Nic Siebenaller <n.7aller@gmail.com>
2018-03-04 07:01:05 -0500

    Added a couple more things

--- noSleepForTheWeek.py
+++ noSleepForTheWeek.py
@@ -1,19 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from user import User
 
 app = Flask(__name__)
+user = User()
 
 
 @app.route('/')
+@app.route('/#')
 def index():
+    """Renders the home page."""
     return render_template(
         'index.html'
     )
 
-@app.route('/home')
-def home():
-    """Renders the home page."""
+
+@app.route('/user', methods=['GET', 'POST'])
+def userSettings():
+    """Renders the alarm settings page."""
+    if request.method == 'POST':
+        user = User(email=request.form['email'], age=request.form['age'])
     return render_template(
-        'layout.html'
+        'alarm-settings.html', username=user
     )
 
 
--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 class User:
-    def __init__(self, age = 30, average_heart_rate = 80, alarm = None):
+    def __init__(self, email = '', age = 30, average_heart_rate = 80, alarm = None):
+        self.email = email
         self.age = age
         self.average_heart_rate = average_heart_rate
         self.alarm = alarm
